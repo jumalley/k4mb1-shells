994ac2a1c7315c7f6b300cad11b40934ea2fea14
Quasar <91174081+quasar-scripts@users.noreply.github.com>
2022-05-17 08:50:13 -0300

    All K4MB1 Shells

---
+++ k4mb1.lua
@@ -0,0 +1,498 @@
+--[[
+    --K4mb1 Default Shells V2 https://www.k4mb1maps.com/package/5015832
+    {
+        nameTier = `default_housing1_k4mb1`,
+        numberTier = 1,
+        offset = json.decode('{"x": -2.32, "y": -0.63, "z": 1.60, "h": 272.87}'),
+        label = 'Default'
+    },
+    {
+        nameTier = `default_housing2_k4mb1`,
+        numberTier = 2,
+        offset = json.decode('{"x": -5.14, "y": -5.05, "z": 3.18, "h": 270.61}'),
+        label = 'Default II'
+    },
+    {
+        nameTier = `default_housing3_k4mb1`,
+        numberTier = 3,
+        offset = json.decode('{"x": -1.35, "y": -2.06, "z": 1.11, "h": 1.14}'),
+        label = 'Default III'
+    },
+    {
+        nameTier = `default_housing4_k4mb1`,
+        numberTier = 4,
+        offset = json.decode('{"x": 0.13, "y": -3.85, "z": 1.09, "h": 1.71}'),
+        label = 'Default IV'
+    },
+    {
+        nameTier = `default_housing5_k4mb1`,
+        numberTier = 5,
+        offset = json.decode('{"x": 1.42, "y": -14.34, "z": 1.14, "h": 0.87}'),
+        label = 'Default V'
+    },
+    {
+        nameTier = `default_housing6_k4mb1`,
+        numberTier = 6,
+        offset = json.decode('{"x": 4.75, "y": -6.49, "z": 1.03, "h": 359.60}'),
+        label = 'Default VI'
+    },
+    --K4mb1 Deluxe Shells V2 https://www.k4mb1maps.com/package/5043817
+    {
+        nameTier = `deluxe_housing1_k4mb1`,
+        numberTier = 7,
+        offset = json.decode('{"x": -22.28, "y": -0.45, "z": 7.26, "h": 268.97}'),
+        label = 'Deluxe'
+    },
+    {
+        nameTier = `deluxe_housing2_k4mb1`,
+        numberTier = 8,
+        offset = json.decode('{"x": -10.30, "y": 0.87, "z": 6.55, "h": 274.91}'),
+        label = 'Deluxe II'
+    },
+    {
+        nameTier = `deluxe_housing3_k4mb1`,
+        numberTier = 9,
+        offset = json.decode('{"x": -9.37, "y": 5.66, "z": 1.08, "h": 270.04}'),
+        label = 'Deluxe III'
+    },
+    --K4mb1 Highend Shells V2 https://www.k4mb1maps.com/package/5043819
+    {
+        nameTier = `highend_housing1_k4mb1`,
+        numberTier = 10,
+        offset = json.decode('{"x": -2.22, "y": 9.02, "z": 8.69, "h": 182.64}'),
+        label = 'Highend'
+    },
+    {
+        nameTier = `highend_housing2_k4mb1`,
+        numberTier = 11,
+        offset = json.decode('{"x": -2.22, "y": 8.97, "z": 8.69, "h": 171.95}'),
+        label = 'Highend II'
+    },
+    {
+        nameTier = `highend_housing3_k4mb1`,
+        numberTier = 12,
+        offset = json.decode('{"x": 11.48, "y": 4.50, "z": 6.42, "h": 128.15}'),
+        label = 'Highend III'
+    },
+    --K4mb1 Medium Shells V2 https://www.k4mb1maps.com/package/5043821
+    {
+        nameTier = `medium_housing1_k4mb1`,
+        numberTier = 13,
+        offset = json.decode('{"x": -0.24, "y": -5.66, "z": 1.71, "h": 1.5}'),
+        label = 'Medium'
+    },
+    {
+        nameTier = `medium_housing2_k4mb1`,
+        numberTier = 14,
+        offset = json.decode('{"x": 6.04, "y": 0.34, "z": 1.03, "h": 357.99}'),
+        label = 'Medium II'
+    },
+    {
+        nameTier = `medium_housing3_k4mb1`,
+        numberTier = 15,
+        offset = json.decode('{"x": 3.42, "y": 1.18, "z": 1.01, "h": 274.17}'),
+        label = 'Medium III'
+    },
+    --K4mb1 Modern Shells V2 https://www.k4mb1maps.com/package/5043818
+    {
+        nameTier = `modern_housing1_k4mb1`,
+        numberTier = 16,
+        offset = json.decode('{"x": 4.29, "y": 10.52, "z": 6.30, "h": 178.92}'),
+        label = 'Modern'
+    },
+    {
+        nameTier = `modern_housing2_k4mb1`,
+        numberTier = 17,
+        offset = json.decode('{"x": -1.76, "y": 10.37, "z": 6.30, "h": 184.71}'),
+        label = 'Modern II'
+    },
+    {
+        nameTier = `modern_housing3_k4mb1`,
+        numberTier = 18,
+        offset = json.decode('{"x": -3.39, "y": -1.45, "z": 5.65, "h": 90.77}'),
+        label = 'Modern III'
+    },
+    --K4mb1 Classic Housing Shells V1 https://www.k4mb1maps.com/package/4673140
+    {
+        nameTier = `classichouse_shell`,
+        numberTier = 19,
+        offset = json.decode('{"x": 4.78, "y": -2.11, "z": 5.26, "h": 87.93}'),
+        label = 'Classic House'
+    },
+    {
+        nameTier = `classichouse2_shell`,
+        numberTier = 20,
+        offset = json.decode('{"x": 4.78, "y": -2.09, "z": 5.26, "h": 90.58}'),
+        label = 'Classic House II'
+    },
+    {
+        nameTier = `classichouse3_shell`,
+        numberTier = 21,
+        offset = json.decode('{"x": 4.78, "y": -2.12, "z": 5.26, "h": 91.60}'),
+        label = 'Classic House III'
+    }, --K4mb1 Deluxe Housing Shells V1 https://www.k4mb1maps.com/package/4673159
+    {
+        nameTier = `shell_highend`,
+        numberTier = 22,
+        offset = json.decode('{"x": -22.37, "y": -0.33, "z": 7.26, "h": 267.73}'),
+        label = 'Highend'
+    },
+    {
+        nameTier = `shell_highendv2`,
+        numberTier = 23,
+        offset = json.decode('{"x": -10.51, "y": 0.86, "z": 6.56, "h": 270.38}'),
+        label = 'Highend II'
+    },
+    {
+        nameTier = `shell_michael`,
+        numberTier = 24,
+        offset = json.decode('{"x": -9.49, "y": 5.54, "z": 9.91, "h": 270.86}'),
+        label = 'Michael'
+    },
+    --K4mb1 Empty Hotel Shells https://www.k4mb1maps.com/package/4811134
+    {
+        nameTier = `k4_hotel1_shell`,
+        numberTier = 25,
+        offset = json.decode('{"x": 4.95, "y": 4.38, "z": 1.16, "h": 176.01}'),
+        label = 'Empty Hotel'
+    },
+    {
+        nameTier = `k4_hotel2_shell`,
+        numberTier = 26,
+        offset = json.decode('{"x": 4.95, "y": 4.38, "z": 1.16, "h": 176.01}'),
+        label = 'Empty Hotel II'
+    },
+    {
+        nameTier = `k4_hotel3_shell`,
+        numberTier = 27,
+        offset = json.decode('{"x": 4.95, "y": 4.38, "z": 1.16, "h": 176.01}'),
+        label = 'Empty Hotel III'
+    },
+    --K4mb1 Empty Motel Shells https://www.k4mb1maps.com/package/4811137
+    {
+        nameTier = `k4_motel1_shell`,
+        numberTier = 28,
+        offset = json.decode('{"x": -9.49, "y": 5.54, "z": 9.91, "h": 270.86}'),
+        label = 'Empty Motel'
+    },
+    {
+        nameTier = `k4_motel2_shell`,
+        numberTier = 29,
+        offset = json.decode('{"x": -9.49, "y": 5.54, "z": 9.91, "h": 270.86}'),
+        label = 'Empty Motel II'
+    },
+    {
+        nameTier = `k4_motel3_shell`,
+        numberTier = 30,
+        offset = json.decode('{"x": -9.49, "y": 5.54, "z": 9.91, "h": 270.86}'),
+        label = 'Empty Motel III'
+    },
+    --K4mb1 Furnished Modern Hotels https://www.k4mb1maps.com/package/4672290
+    {
+        nameTier = `modernhotel_shell`,
+        numberTier = 31,
+        offset = json.decode('{"x": 4.87, "y": 4.38, "z": 1.16, "h": 176.40}'),
+        label = 'Hotel Furnished'
+    },
+    {
+        nameTier = `modernhotel2_shell`,
+        numberTier = 32,
+        offset = json.decode('{"x": 4.87, "y": 4.38, "z": 1.16, "h": 176.40}'),
+        label = 'Hotel II Furnished'
+    },
+    {
+        nameTier = `modernhotel3_shell`,
+        numberTier = 33,
+        offset = json.decode('{"x": 4.95, "y": 4.38, "z": 1.16, "h": 176.01}'),
+        label = 'Hotel III Furnished'
+    },
+    --K4mb1 Furnished Housing Shells https://www.k4mb1maps.com/package/4672272
+    {
+        nameTier = `furnitured_lowapart`,
+        numberTier = 34,
+        offset = json.decode('{"x": 5.05, "y": -1.39, "z": 3.0, "h": 357.14}'),
+        label = 'Furnished Low Apartment'
+    },
+    {
+        nameTier = `furnitured_midapart`,
+        numberTier = 35,
+        offset = json.decode('{"x": 1.46, "y": -10.33, "z": 1.06, "h": 0.39}'),
+        label = 'Furnished Mid Apartment'
+    },
+    {
+        nameTier = `furnitured_motel`,
+        numberTier = 36,
+        offset = json.decode('{"x": -1.51, "y": -3.99, "z": 1.08, "h": 1.28}'),
+        label = 'Furnished Motel'
+    },
+    --K4mb1 Furnished Motel Shells  https://www.k4mb1maps.com/package/4672296
+    {
+        nameTier = `classicmotel_shell`,
+        numberTier = 37,
+        offset = json.decode('{"x": 0.045, "y": -3.707, "z": 1.05, "h": 351.86}'),
+        label = 'Furnished Motel Classic'
+    },
+    {
+        nameTier = `highendmotel_shell`,
+        numberTier = 38,
+        offset = json.decode('{"x": 3.21, "y": 3.50, "z": 1.16, "h": 178.23}'),
+        label = 'Furnished Motel Highend'
+    },
+    {
+        nameTier = `standardmotel_shell`,
+        numberTier = 39,
+        offset = json.decode('{"x": -0.43, "y": -2.51, "z": 1.0, "h": 271.29}'),
+        label = 'Furnished Motel Standard'
+    },
+    --K4mb1 Highend Housing Shells V1 https://www.k4mb1maps.com/package/4673131
+    {
+        nameTier = `shell_apartment1`,
+        numberTier = 40,
+        offset = json.decode('{"x": -2.23, "y": 9.01, "z": 8.69, "h": 178.81}'),
+        label = 'Highend'
+    },
+    {
+        nameTier = `shell_apartment2`,
+        numberTier = 41,
+        offset = json.decode('{"x": -2.25, "y": 9.00, "z": 8.69, "h": 177.86}'),
+        label = 'Highend II'
+    },
+    {
+        nameTier = `shell_apartment3`,
+        numberTier = 42,
+        offset = json.decode('{"x": 11.75, "y": 4.55, "z": 8.13, "h": 129.16}'),
+        label = 'Highend III'
+    },
+    --K4mb1 Mansion Housing Shells https://www.k4mb1maps.com/package/4783251
+    {
+        nameTier = `k4_mansion_shell`,
+        numberTier = 43,
+        offset = json.decode('{"x": -0.32, "y": -0.68, "z": 7.86, "h": 178.98}'),
+        label = 'Mansion'
+    },
+    {
+        nameTier = `k4_mansion2_shell`,
+        numberTier = 44,
+        offset = json.decode('{"x": -0.32, "y": -0.57, "z": 7.86, "h": 178.74}'),
+        label = 'Mansion II'
+    },
+    {
+        nameTier = `k4_mansion3_shell`,
+        numberTier = 45,
+        offset = json.decode('{"x": -0.18, "y": -0.57, "z": 7.86, "h": 180.76}'),
+        label = 'Mansion III'
+    },
+    --K4mb1 Medium Housing Shells V1 https://www.k4mb1maps.com/package/4672307
+    {
+        nameTier = `shell_frankaunt`,
+        numberTier = 46,
+        offset = json.decode('{"x": -0.36, "y": -5.89, "z": 1.70, "h": 358.21}'),
+        label = 'Frank Aunt'
+    },
+    {
+        nameTier = `shell_medium2`,
+        numberTier = 47,
+        offset = json.decode('{"x": 6.04, "y": 0.34, "z": 1.03, "h": 357.99}'),
+        label = 'Medium II'
+    },
+    {
+        nameTier = `shell_medium3`,
+        numberTier = 48,
+        offset = json.decode('{"x": 3.32, "y": 1.23, "z": 2.57, "h": 273.46}'),
+        label = 'Medium III'
+    },
+    --K4mb1 Modern Housing Shells V1 https://www.k4mb1maps.com/package/4673169
+    {
+        nameTier = `shell_banham`,
+        numberTier = 49,
+        offset = json.decode('{"x": -3.26, "y": -1.63, "z": 6.25, "h": 90.49}'),
+        label = 'Banham'
+    },
+    {
+        nameTier = `shell_westons`,
+        numberTier = 50,
+        offset = json.decode('{"x": 4.29, "y": 10.59, "z": 6.95, "h": 183.60}'),
+        label = 'Westons'
+    },
+    {
+        nameTier = `shell_westons2`,
+        numberTier = 51,
+        offset = json.decode('{"x": -1.76, "y": 10.62, "z": 6.95, "h": 179.20}'),
+        label = 'Westons II'
+    },
+    --K4mb1 Garage Shells https://www.k4mb1maps.com/package/4673177
+    {
+        nameTier = `shell_garagel`,
+        numberTier = 52,
+        offset = json.decode('{"x": 12.02, "y": -14.30, "z": 0.99, "h": 89.42}'),
+        label = 'Garage Large'
+    },
+    {
+        nameTier = `shell_garagem`,
+        numberTier = 53,
+        offset = json.decode('{"x": 13.90, "y": 1.63, "z": 1.0, "h": 87.05}'),
+        label = 'Garage Medium'
+    },
+    {
+        nameTier = `shell_garages`,
+        numberTier = 54,
+        offset = json.decode('{"x": 5.85, "y": 3.86, "z": 1.0, "h": 180.05}'),
+        label = 'Garage Small'
+    },
+    --K4mb1 Office Shells https://www.k4mb1maps.com/package/4673258
+    {
+        nameTier = `shell_office1`,
+        numberTier = 55,
+        offset = json.decode('{"x": 1.88, "y": 5.06, "z": 2.05, "h": 180.07}'),
+        label = 'Office'
+    },
+    {
+        nameTier = `shell_office2`,
+        numberTier = 56,
+        offset = json.decode('{"x": 3.66, "y": -1.94, "z": 1.26, "h": 92.73}'),
+        label = 'Office II'
+    },
+    {
+        nameTier = `shell_officebig`,
+        numberTier = 57,
+        offset = json.decode('{"x": -12.48, "y": 1.91, "z": 5.30, "h": 175.13}'),
+        label = 'Office Big'
+    },
+    --K4mb1 Furnished Stash House Shells  https://www.k4mb1maps.com/package/4672293
+    {
+        nameTier = `container2_shell`,
+        numberTier = 58,
+        offset = json.decode('{"x": 0.02, "y": -5.37, "z": 1.12, "h": 355.28}'),
+        label = 'Container'
+    },
+    {
+        nameTier = `stashhouse1_shell`,
+        numberTier = 59,
+        offset = json.decode('{"x": 21.41, "y": -0.52, "z": 19.33, "h": 85.84}'),
+        label = 'Stash house'
+    },
+    {
+        nameTier = `stashhouse3_shell`,
+        numberTier = 60,
+        offset = json.decode('{"x": -0.003, "y": 5.5, "z": 3.04, "h": 180.77}'),
+        label = 'Stash house II'
+    },
+    --K4mb1 Stash House Shells https://www.k4mb1maps.com/package/4673273
+    {
+        nameTier = `stashhouse_shell`,
+        numberTier = 61,
+        offset = json.decode('{"x": 20.88, "y": -0.40, "z": 15.42, "h": 86.54}'),
+        label = 'Stash house'
+    },
+    {
+        nameTier = `stashhouse2_shell`,
+        numberTier = 62,
+        offset = json.decode('{"x": -1.98, "y": 2.26, "z": 1.0, "h": 263.81}'),
+        label = 'Stash house II'
+    },
+    {
+        nameTier = `container_shell`,
+        numberTier = 63,
+        offset = json.decode('{"x": 0.08, "y": -5.73, "z": 1.24, "h": 359.32}'),
+        label = 'Container'
+    },
+    --K4mb1 Store Shells https://www.k4mb1maps.com/package/4673264
+    {
+        nameTier = `shell_barber`,
+        numberTier = 64,
+        offset = json.decode('{"x": 1.54, "y": 5.40, "z": 1.0, "h": 175.27}'),
+        label = 'Barber'
+    },
+    {
+        nameTier = `shell_gunstore`,
+        numberTier = 65,
+        offset = json.decode('{"x": -1.02, "y": -5.43, "z": 1.03, "h": 359.77}'),
+        label = 'Gunstore'
+    },
+    {
+        nameTier = `shell_store1`,
+        numberTier = 66,
+        offset = json.decode('{"x": -2.61, "y": -4.73, "z": 1.08, "h": 1.0}'),
+        label = 'General store'
+    },
+    {
+        nameTier = `shell_store2`,
+        numberTier = 67,
+        offset = json.decode('{"x": -0.64, "y": -5.07, "z": 1.02, "h": 1.91}'),
+        label = 'General store II'
+    },
+    {
+        nameTier = `shell_store3`,
+        numberTier = 68,
+        offset = json.decode('{"x": -0.14, "y": -7.87, "z": 2.01, "h": 358.15}'),
+        label = 'General store III'
+    },
+    --K4mb1 Warehouse Shells https://www.k4mb1maps.com/package/4673185
+    {
+        nameTier = `shell_warehouse1`,
+        numberTier = 69,
+        offset = json.decode('{"x": -8.95, "y": 0.51, "z": 1.04, "h": 268.82}'),
+        label = 'Warehouse'
+    },
+    {
+        nameTier = `shell_warehouse2`,
+        numberTier = 70,
+        offset = json.decode('{"x": 12.51, "y": -0.01, "z": 1.03, "h": 94.52}'),
+        label = 'Warehouse II'
+    },
+    {
+        nameTier = `shell_warehouse3`,
+        numberTier = 71,
+        offset = json.decode('{"x": 2.61, "y": -1.65, "z": 1.00, "h": 85.2}'),
+        label = 'Warehouse III'
+    },
+    --K4mb1 Highend Lab Shells https://www.k4mb1maps.com/package/4698329
+    {
+        nameTier = `k4coke_shell`,
+        numberTier = 72,
+        offset = json.decode('{"x": -11.06, "y": -2.52, "z": 22.64, "h": 272.51}'),
+        label = 'Highend Cocaine Lab Furnished'
+    },
+    {
+        nameTier = `k4meth_shell`,
+        numberTier = 73,
+        offset = json.decode('{"x": -11.06, "y": -2.48, "z": 9.47, "h": 277.54}'),
+        label = 'Highend Meth Lab Furnished'
+    },
+    {
+        nameTier = `k4weed_shell`,
+        numberTier = 74,
+        offset = json.decode('{"x": -11.05, "y": -2.50, "z": 20.96, "h": 283.97}'),
+        label = 'Highend Weed Lab Furnished'
+    },
+    --K4mb1 Drug Lab Shells https://www.k4mb1maps.com/package/4672285
+    {
+        nameTier = `shell_coke1`,
+        numberTier = 75,
+        offset = json.decode('{"x": -6.24, "y": 8.48, "z": 1.00, "h": 179.30}'),
+        label = 'Cocaine Lab Furnished'
+    },
+    {
+        nameTier = `shell_coke2`,
+        numberTier = 76,
+        offset = json.decode('{"x": -6.32, "y": 8.60, "z": 1.03, "h": 179.23}'),
+        label = 'Cocaine Lab II Furnished'
+    },
+    {
+        nameTier = `shell_meth`,
+        numberTier = 77,
+        offset = json.decode('{"x": -6.39, "y": 8.54, "z": 1.03, "h": 178.84}'),
+        label = 'Meth Lab Furnished'
+    },
+    {
+        nameTier = `shell_weed`,
+        numberTier = 78,
+        offset = json.decode('{"x": 17.46, "y": 11.71, "z": 1.01, "h": 88.37}'),
+        label = 'Weed Lab Furnished'
+    },
+    {
+        nameTier = `shell_weed2`,
+        numberTier = 79,
+        offset = json.decode('{"x": 17.85, "y": 11.75, "z": 1.01, "h": 88.11}'),
+        label = 'Weed Lab II Furnished'
+    },  ]]
